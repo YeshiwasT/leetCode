43311daf51dcb364fe2e929feddb9658f7c30a6b
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2022-01-17 10:42:22 +0300

    Remove_Duplicates_from_Sorted_Array

---
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,26 @@
+public class Remove_Duplicates_from_Sorted_Array {
+    //I have to push it 
+        public int removeDuplicates(int[] nums) {
+            int j=0;
+            if(nums.length==0){
+                return 0;
+            }else{
+            for(int i=0;i<nums.length-1;i++){
+                if(nums[i]!=nums[i+1]){
+                    
+                    nums[j]=nums[i];
+                    j++;
+                    
+                }
+                else{
+                    continue;
+                }
+            }
+            nums[j]=nums[nums.length-1];
+            j++;
+            return j;
+            
+        }}
+    
+    
+}
