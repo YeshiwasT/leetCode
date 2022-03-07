1bb9b4cb7b14e7342ba71339b3551340b2e796de
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2022-03-07 21:48:44 +0300

    Number of Good Pairs

---
+++ Number_of_Good_Pairs.java
@@ -0,0 +1,19 @@
+public class Number_of_Good_Pairs {
+    
+   public static int  numIdenticalPairs(int[] nums) {
+          int count=0;
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = i+1; j < nums.length; j++) {
+            if (nums[i]==nums[j]) {
+                count++;
+            }
+        }
+    }
+       return count;
+   } 
+   public static void main(String[] args) {
+    int[] nums={1,1,1,1};
+   System.out.println( numIdenticalPairs(nums));
+ 
+   }
+}
