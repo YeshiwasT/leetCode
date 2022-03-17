2552a9daddc1544d1b1ffcde69132c4360e5f186
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2022-03-17 16:42:07 +0300

    Max Number ofK-SumPairs

---
+++ Max_Number_of_KSum_Pairs.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class Max_Number_of_KSum_Pairs {
+ 
+    public int maxOperations(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        int maxOp=0;
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                continue;
+            }
+            int temp=k-nums[i];
+            if(map.containsKey(temp)){
+                int j=i+1;
+                while(j<nums.length){
+                    if(temp==nums[j]){
+                    nums[j]=0;
+                         maxOp++;
+
+                        break;
+                    
+                    }
+                j++;}
+                nums[i]=0;
+
+                
+            }
+        }
+      return maxOp;  
+    }
+}
