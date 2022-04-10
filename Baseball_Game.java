dc9d9606d40e9cbeef0818acf8f6fd68d21dd9e7
YeshiwasT <81110077+YeshiwasT@users.noreply.github.com>
2022-04-10 14:41:20 +0300

    Baseball Game

---
+++ Baseball_Game.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class Baseball_Game{
+    
+        public int calPoints(String[] ops) {
+            Stack<String> stack=new Stack<>();
+            if (ops.length==1){
+                return Integer.parseInt(ops[0]);
+            }
+            for(int i=0;i<ops.length;i++){
+                if(!stack.isEmpty()&&ops[i].equals("C")){
+                    stack.pop();
+                }else if(!stack.isEmpty()&&ops[i].equals("D")){
+                   
+                    stack.push(Integer.toString(2*Integer.parseInt(stack.peek())));
+                }
+           else if(stack.size()>=2&&ops[i].equals("+")){
+                 String temp1=   stack.pop();
+                            String temp2=   stack.peek();
+               stack.push(temp1); 
+               stack.push(Integer.toString(Integer.parseInt(temp1)+Integer.parseInt(temp2)));public class Baseball_Game {
+                   
+               }
+               
+                }else {
+               try {
+     
+                   stack.push(Integer.toString(Integer.parseInt(ops[i])));
+    
+    } catch (NumberFormatException e) {
+                   continue;
+      
+    }
+           }}
+           int result=0;
+                          while(!stack.isEmpty()){
+                         
+                              result=result+Integer.parseInt(stack.pop());
+                          }
+            return result;
+        }
+    }
+}
