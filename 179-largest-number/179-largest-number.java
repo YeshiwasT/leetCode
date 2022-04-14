e032fd6ffed3c24bae48bc98209df106412b19d9
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-14 18:51:33 +0300

    Time: 8 ms (74.58%), Space: 41.9 MB (90.29%) - LeetHub

---
+++ 179-largest-number/179-largest-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+       String[] sArr=new String[nums.length];
+        for(int i=0;i<sArr.length;i++){
+            sArr[i]=Integer.toString(nums[i]);
+        }
+       Arrays.sort(sArr,(a,b)->(b+a).compareTo(a+b));
+              //  Arrays.sort(sArr);
+       System.out.println( Arrays.toString(sArr));
+        if(sArr[0].charAt(0)=='0'){
+            return "0";
+        }
+               StringBuilder str=new StringBuilder();
+        for(int i=0;i<sArr.length;i++){
+            str.append(sArr[i]);
+        }
+        
+        return str.toString();
+    }
+}
