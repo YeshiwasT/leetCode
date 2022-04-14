d6fcafa5e9c1cab7a8e54f35619fa04d2994c61f
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-14 18:54:15 +0300

    Time: 5 ms (88.29%), Space: 41.9 MB (87.37%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -5,17 +5,14 @@ class Solution {
             sArr[i]=Integer.toString(nums[i]);
         }
        Arrays.sort(sArr,(a,b)->(b+a).compareTo(a+b));
-              //  Arrays.sort(sArr);
-       System.out.println( Arrays.toString(sArr));
         if(sArr[0].charAt(0)=='0'){
             return "0";
         }
-               StringBuilder str=new StringBuilder();
-        String std="";
+        String str="";
         for(int i=0;i<sArr.length;i++){
-            std=std+sArr[i];
+            str=str+sArr[i];
         }
         
-        return std;
+        return str;
     }
 }
