4d24db9ae268cf3bab367cf26095dde2fdee371b
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-16 10:52:34 +0300

    Time: 1 ms (83.72%), Space: 42.7 MB (83.27%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -17,41 +17,25 @@ class Solution {
             int ans=0;
             int leftVal=0;
     public TreeNode convertBST(TreeNode root) {
-       int total= totalSum(root);
-        
-            return changed(root,total);
+          int total= totalSum(root);
+          return changed(root,total);
     }
     public int totalSum(TreeNode root){
           if(root==null){
               return ans;
           }
-//          if(root!=null){
-//              ans=ans+root.val;
-//              return ans;
-//          }
-     //   System.out.println(root.val+ans+"  "+root.val);
-                     ans=ans+root.val;
+          ans=ans+root.val;
          totalSum(root.left);
-
-                     
-
-        totalSum(root.right);
-
-        return ans;
+         totalSum(root.right);
+    return ans;
     }
     public TreeNode changed(TreeNode root,int total){
         if(root==null)return null;
-                    //  total=total-root.val;
-
-      root.left=changed(root.left,total);
-        int temp=root.val;
-                           root.val=(total)-leftVal;
-
-                        leftVal+=temp;
-
-                System.out.println(root.val+" "+total+" "+leftVal);
-                   //root.val=total-leftVal;
-      root.right=changed(root.right,total);
+            root.left=changed(root.left,total);
+            int temp=root.val;
+            root.val=(total)-leftVal;
+            leftVal+=temp;
+            root.right=changed(root.right,total);
         return root;
         
     }
