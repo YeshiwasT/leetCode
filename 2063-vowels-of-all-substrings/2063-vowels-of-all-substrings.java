4860861e3eea6cd577e2b9bc1db405a72fbe8790
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-16 18:01:08 +0300

    Time: 17 ms (42.38%), Space: 43.5 MB (78.12%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.java
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.java
@@ -1,13 +1,13 @@
 class Solution {
     public long countVowels(String word) {
-        long result=0;
- long len=word.length();
+         long result=0;
+         long len=word.length();
         for(int i=0;i<word.length();i++){
-if(word.charAt(i)=='i'||word.charAt(i)=='o'||word.charAt(i)=='a'||word.charAt(i)=='e'||word.charAt(i)=='u'){        
+             if(word.charAt(i)=='i'||word.charAt(i)=='o'||word.charAt(i)=='a'||
+                         word.charAt(i)=='e'||word.charAt(i)=='u')
+             {        
              result+=(len-i)*(i+1);
-             
-         }
-     }
-        return result;
+              } }
+          return result;
     }
 }
