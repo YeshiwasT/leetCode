24e7999399fbbf89054e520c2f4c8bb16ca2f225
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-19 23:59:31 +0300

    Time: 8 ms (7.67%), Space: 47.1 MB (58.97%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -18,9 +18,6 @@ class Solution {
      ArrayList<TreeNode> list=new ArrayList<>();
     public void recoverTree(TreeNode root) {
         helper(root);
-         for(int i=0;i<list.size();i++){
-             System.out.print(list.get(i).val);
-         }
         for(int i=0;i<list.size();i++){
             int min=i;
             for(int j=i+1;j<list.size();j++){
@@ -33,13 +30,7 @@ class Solution {
                    list.get(min).val=list.get(i).val;
                    list.get(i).val=temp;
                    break;
-               } 
-        }
-        System.out.println();
-         for(int i=0;i<list.size();i++){
-             System.out.print(list.get(i).val);
-         }
-        
+               }  }
     }
     public void helper(TreeNode root){
         if(root==null)return ;
@@ -47,51 +38,4 @@ class Solution {
         list.add(root);
         helper(root.right);
     }
-    
-//          if(root==null)return ;
-//         int current=root.val;
-//         minRoot(root.left,current);
-//         maxRoot(root.right,current);
-
-//          if(current<tempMin){
-//             root.val=tempMin;}
-//     else if(current>tempMax){
-        
-//         root.val=tempMax;
-    
-//     }
-//             recoverTree(root.left);
-//             recoverTree(root.left);
-
-//     }
-   
-//     public void minRoot(TreeNode root,int current){
-//         if(root==null)return ;
-    
-//         if(current<root.val)
-//        {    
-//            tempMin=root.val;
-//            root.val=current;
-//           // return tempMin;
-//           // return ;
-//        }
-//     minRoot(root.left,current);
-//     minRoot(root.right,current);
-//    // return   tempMin;
-        
-//     }
-//      public void maxRoot(TreeNode root,int current){
-//          if(root==null)return ;
-         
-//           if(current>root.val){
-//              tempMax=root.val;
-//            root.val=current;
-//            //   return;
-              
-//           // return tempMax;
-//         }
-//            maxRoot(root.left,current);
-//      maxRoot(root.right,current);
-//        // return tempMax;
-  //  }
 }
