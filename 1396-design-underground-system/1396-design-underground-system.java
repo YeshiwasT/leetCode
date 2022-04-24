05939cbc483e0ae46a4e97255b49fd313b0b823d
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-24 09:04:38 +0300

    Time: 236 ms (10.58%), Space: 109.4 MB (5.05%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -0,0 +1,51 @@
+class UndergroundSystem {
+          HashMap<Integer,String[]> map;
+              HashMap<String,double[]> mapAvarage;
+
+    public UndergroundSystem() {
+       map=new HashMap<>();
+               mapAvarage=new HashMap<>();
+
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+        String[] arr={stationName,Integer.toString(t),Integer.toString(1)};
+        map.put(id,arr);
+        
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+        int totalTime=t-Integer.valueOf(map.get(id)[1]);
+   String key=map.get(id)[0]+2+stationName;
+
+        if(mapAvarage.containsKey(key)){
+            String [] arr=map.get(id);
+            arr[2]=Integer.toString(Integer.valueOf(map.get(id)[2])+1);
+            map.put(id,arr);
+         double totalAvarage=(mapAvarage.get(key)[0]+totalTime);
+         double[] dArr={totalAvarage,Double.valueOf(mapAvarage.get(key)[1]+1)};
+      mapAvarage.put(key,dArr);
+
+        }else{
+                     double[] dArr={Double.valueOf(totalTime),1};
+
+               mapAvarage.put(key,dArr);
+   
+        }
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        String key=startStation+2+endStation;
+        System.out.println(mapAvarage.get(key)[1]);
+        return mapAvarage.get(key)[0]/mapAvarage.get(key)[1];
+        
+    }
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
