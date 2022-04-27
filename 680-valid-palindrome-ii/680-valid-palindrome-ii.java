53c0746bb77765864162d423b155b72db8278fd0
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-04-27 13:31:09 +0300

    Time: 41 ms (5.05%), Space: 43.4 MB (79.06%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+      StringBuilder sBuild=new StringBuilder(s);
+    
+      String sBuildI="as";
+     String sBuildN="fg";
+     String sBuildIR="gr";
+     String sBuildNR="ty";
+
+     int   n=s.length()-1;
+        for(int i=0;i<sBuild.length();i++){
+            if(n>0&&sBuild.charAt(i)!=sBuild.charAt(n)){
+                if(sBuild.charAt(i+1)==sBuild.charAt(n)){
+                    StringBuilder forI=new StringBuilder(s);
+                                   forI.deleteCharAt(i);
+                   sBuildI=forI.toString();
+                    sBuildIR=forI.reverse().toString();
+                                    System.out.println(sBuildI+" "+sBuildIR);
+
+                }
+                if(sBuild.charAt(i)==sBuild.charAt(n-1)){
+                      StringBuilder forN=new StringBuilder(s);
+                   forN.deleteCharAt(n);
+                
+                   sBuildN=forN.toString();
+                 sBuildNR=forN.reverse().toString();
+               System.out.println(sBuildN+" "+sBuildNR);
+
+
+                }
+               
+                  break;
+            }
+            n--;
+        }
+      //  System.out.println(sBuild);
+        if(sBuildI.equals(sBuildIR)||sBuildN.equals(sBuildNR)||s.equals(sBuild.reverse().toString())){
+            return true;
+        }
+    
+    return false;}
+}
