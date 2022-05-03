44c858142a03244f1e512159ab0323ccec4c1b0f
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-05-03 22:17:47 +0300

    Time: 15 ms (9.53%), Space: 42.8 MB (20.08%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -3,29 +3,16 @@ class Solution {
         String s=strs[0];
         String k="";
         for(int i=1;i<strs.length;i++){
-            StringBuilder sb=new StringBuilder(strs[i]);
-           
-                for(int j=0;j<strs[i].length()&&j<s.length();j++){
+           for(int j=0;j<strs[i].length()&&j<s.length();j++){
                     if(s.charAt(j)==strs[i].charAt(j)){
                         k+=s.charAt(j);
                     }else{
                         if(s.charAt(0)!=strs[i].charAt(0)){
-                                    return "";
-    
-                        }
-                        break;
-                        //return "";
-                    }
-                }
-                System.out.println(s);
-               
-                s=k;
-                k="";
-            
-            
-        }
+                                    return ""; }
+                        break;}}
+                    s=k;
+                    k=""; }
       
-        
-        return s;
+       return s;
     }
 }
