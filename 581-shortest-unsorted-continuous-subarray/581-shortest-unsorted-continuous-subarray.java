aad667cdf58bd46c03be978d32ba203f34db94ed
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-05-04 18:50:30 +0300

    Time: 10 ms (32.40%), Space: 54.7 MB (10.74%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -19,35 +19,6 @@ class Solution {
                 
             }
         }
-//         Stack<Integer> stack=new Stack<>();
-//         ArrayList<Integer> list=new ArrayList<>();
-//         ArrayList<Integer> list2=new ArrayList<>();
-//         int count=0;
-//         for(int i=0;i<nums.length;i++){
-//             if(list2.size()>0&&list2.get(list2.size()-1)>nums[i]){
-//                 list.add(i);
-//                 list2.add(nums[i]);
-//                 Collections.sort(list2);
-// //                 while(!stack.isEmpty()&&stack.peek()>=nums[i]){
-                    
-// //                    // if(stack.peek()>nums[i]){
-// //                        // stack.pop();4
-                    
-// //                         count++;
-// //                     //}
-// //                 }
-//                 stack.push(nums[i]);
-//             }
-//             else{
-//                 list2.add(nums[i]);
-//                 stack.push(nums[i]);
-//             }
-//         }
-//         if(list.size()>0){
-//                  return list.get(list.size()-1)-list.get(0)+2;
-   
-//         }
-//         return 0;
         return second-first+1;
     }
 }
