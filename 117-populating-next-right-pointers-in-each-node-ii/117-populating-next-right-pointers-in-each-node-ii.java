3261e8a9f444232bf6f187fa64e668fd48af5019
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-05-13 16:42:21 -0400

    Time: 1 ms (71.78%), Space: 45.4 MB (18.40%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -24,45 +24,25 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root==null)return null;
-        Node result=new Node(root.val);
         Queue<Node> queue=new LinkedList<>();
-              //  helper(root);
-
         queue.add(root);
         while(!queue.isEmpty()){
-        // Node temp=new Node(root.val);   
-          //  if(queue.peek()!=null){
          int len=queue.size();
             for(int i=0;i<len;i++){
-                         Node temp= queue.poll();
+                 Node temp= queue.poll();
                 if(i<len-1){
-                  temp.next=queue.peek();
-   
-                }
-
-          if (temp.left != null) {
-                queue.add(temp.left);
-            }
- 
-            if (temp.right != null) {
-                queue.add(temp.right);
+                  temp.next=queue.peek(); }
+                if (temp.left != null) {
+                  queue.add(temp.left);}
+                if (temp.right != null) {
+                   queue.add(temp.right);
             }
             }
            
            
-          //  queue.add(null);
         }
         
         return root;
         
     }
-//     public void helper(Node root){
-//          if(root==null)return;
-//         helper(root.left);
-//         helper(root.right);
-//                 System.out.print(root.val+"k");
-//                 root.next=null;
-
-
-//     }
 }
