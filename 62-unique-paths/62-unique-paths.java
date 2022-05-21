cc279a13cf266d817f90895c37990bcd2651d988
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-05-21 15:54:16 -0400

    Time: 1 ms (41.22%), Space: 40.8 MB (58.65%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -5,19 +5,15 @@ class Solution {
         row=m;
         column=n;
         int[][] dp =new int[m][n];
-     return   helper(dp,0,0);
+        return   helper(dp,0,0);
     }
     
   public int  helper(int[][] dp,int i,int j){
-     if(i==row||j==column)return 0;
-      if(i==row-1&&j==column-1)return 1;
-      
-      if(dp[i][j]==0){
-          return  dp[i][j]= helper(dp,i,j+1)+helper(dp,i+1,j);
-      }
-      else{
+       if(i==row||j==column)return 0;
+        if(i==row-1&&j==column-1)return 1;
+        if(dp[i][j]==0){
+          return  dp[i][j]= helper(dp,i,j+1)+helper(dp,i+1,j);}
+        else{
           return dp[i][j];
-      }
-      
-    }
+           } }
 }
