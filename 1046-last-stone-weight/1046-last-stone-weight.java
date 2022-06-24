e7c50f98adbfa18afdedfc5e3f65619ca0d959e4
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-06-24 22:17:39 +0300

    Time: 1 ms (99.51%), Space: 42 MB (23.96%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        
+        
+        Arrays.sort(stones);
+        if(stones.length==1){
+            return stones[0];
+        }
+            if(stones[stones.length-2]==0){
+                return stones[stones.length-1];
+            }
+        
+            if(stones[stones.length-1]!=0){
+                if(stones[stones.length-1]-stones[stones.length-2]>0){
+                 
+                    stones[stones.length-1]=stones[stones.length-1]-stones[stones.length-2];
+                    stones[stones.length-2]=0;
+                }else{
+                    stones[stones.length-1]=0;
+                    stones[stones.length-2]=0;
+                }
+        }
+        
+         return         lastStoneWeight(stones);
+        
+    }
+  
+}
