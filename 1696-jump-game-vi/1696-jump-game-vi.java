798f9605fc865a4dc21db35ade2d6ff6431bc63f
yeshiwas <81110077+YeshiwasT@users.noreply.github.com>
2022-07-04 20:18:35 +0300

    Time: 76 ms (33.57%), Space: 116.8 MB (12.50%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -2,17 +2,16 @@ class Solution {
     public int maxResult(int[] nums, int k) {
         int[] dp=new int[nums.length];
         Deque<Integer> deque=new LinkedList<>();
-	dp[0] = nums[0];
+	dp[0]=nums[0];
         deque.add(0);
-
-	for(int i = 1; i < nums.length; i++) {
-		if(deque.peekFirst() < i - k) deque.pop();          
-		dp[i] = nums[i] + dp[deque.peekFirst()];     
-		while(!deque.isEmpty() && dp[deque.peekLast()] <= dp[i])   
-		    deque.pollLast();
-		deque.add(i);                              
-	}
-	return dp[nums.length-1];
+        for(int i=1;i<nums.length;i++){
+            if(deque.peekFirst()<i-k)deque.pollFirst();
+            dp[i]=nums[i]+dp[deque.peekFirst()];
+            while(!deque.isEmpty()&&dp[deque.peekLast()]<=dp[i])
+                deque.pollLast();
+            deque.add(i);
+        }
+        return dp[nums.length-1];
     }
 }
  
